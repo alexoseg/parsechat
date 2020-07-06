545115e4db4817af1da0e7ef116a011567eb9d93
alexoseg <aoseg@stanford.edu>
2020-07-06 09:47:14 -0700

    Added login and signup functionality to ParseChat

---
+++ ParseChat/View Controllers/LoginViewController.m
@@ -0,0 +1,85 @@
+//
+//  LoginViewController.m
+//  ParseChat
+//
+//  Created by Alex Oseguera on 7/6/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void) displayAlertWithMessage:(NSString *)alertMessage andTitle:(NSString *)titleMessage{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:titleMessage message:alertMessage preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+      style:UIAlertActionStyleDefault
+    handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    [alert addAction:okAction];
+    [self presentViewController:alert animated:YES completion:^{
+    }];
+}
+
+- (void)registerUser{
+    if([self.usernameTextField.text isEqualToString:@""] || [self.passwordTextField.text isEqualToString:@""]){
+        [self displayAlertWithMessage:@"Make sure to fill username and password fields" andTitle:@"Error: Empty Fields"];
+        return;
+    }
+    
+    PFUser *const newUser = [PFUser user];
+    
+    newUser.username = self.usernameTextField.text;
+    newUser.password = self.passwordTextField.text;
+    
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if(succeeded){
+            NSLog(@"User Registered Successfully");
+        } else {
+            [self displayAlertWithMessage:error.localizedDescription andTitle:@"Sign up error"];
+        }
+    }];
+}
+
+- (void)loginUser {
+    NSString *username = self.usernameTextField.text;
+    NSString *password = self.passwordTextField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * _Nullable user, NSError * _Nullable error) {
+        if(error == nil){
+            NSLog(@"User Registered Successfully");
+        } else {
+            [self displayAlertWithMessage:error.localizedDescription andTitle:@"Login Error"];
+        }
+    }];
+}
+
+- (IBAction)signUpUser:(id)sender {
+    [self registerUser];
+}
+- (IBAction)loginUser:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
